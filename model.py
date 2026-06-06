ea35d4f1ffd213ca927bd84be5df70b77703463d
Wesley1012 <wesleyfirehand@gmail.com>
2026-06-06 23:03:11 +0300

    Added other modules

--- model.py
+++ model.py
@@ -1,19 +1,19 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, constr, Field
 
 class Creature(BaseModel):
-    name: str
-    country: str
+    name: constr(min_length=2)
+    country: str = Field(..., min_length=2) # ... - значение обязательное и значения по умолчанию нет
     area: str
     description: str
     aka: str
 
-thing = Creature(
-    name='yeti',
-    country='CN',
-    area='Himalayas',
-    description='Hirsute Himalayan',
-    aka='Abominable Snowman'
-)
+# thing = Creature(
+#     name='yeti',
+#     country='CN',
+#     area='Himalayas',
+#     description='Hirsute Himalayan',
+#     aka='Abominable Snowman'
+# )
 
 
 
---
+++ test_model.py
@@ -0,0 +1,9 @@
+from model import Creature
+
+dragon = Creature(
+    name='f2',
+    description="['incorrect', 'string', 'list']",
+    country='*',
+    area='*',
+    aka='firedrake'
+)
